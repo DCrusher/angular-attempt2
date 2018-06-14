7d3150cf312b2987cd4e493e08c7fa0e5ac61d36
Ivan Kuzmin <kuzmin6969@gmail.com>
2018-06-14 15:34:09 +0300

    Customize bootstrap styles for CoreUI form

--- src/app/pages/forms-page/coreui-form/coreui-form.model.ts
+++ src/app/pages/forms-page/coreui-form/coreui-form.model.ts
@@ -3,100 +3,114 @@ import {
   DynamicInputModel,
   DynamicSelectModel,
   DynamicTextAreaModel,
+  DynamicFormGroupModel
 } from '@ng-dynamic-forms/core';
 import { of } from 'rxjs/observable/of';
 
 export const SAMPLE_FORM_MODEL = [
-  new DynamicSelectModel<string>({
+  new DynamicFormGroupModel({
+    id: 'first-row',
+    group: [
+      new DynamicSelectModel<string>({
 
-      id: 'demoSelect',
-      label: 'Fee Type',
-      options: of([
-          {
-              label: 'Option 1',
-              value: 'option-1',
-          }
-      ])
+        id: 'demoSelect',
+        label: 'Fee Type',
+        options: of([
+            {
+                label: 'Option 1',
+                value: 'option-1',
+            }
+        ])
+      }),
+      new DynamicInputModel({
+          id: 'demoInput',
+          label: 'Amount',
+          maxLength: 51,
+          placeholder: 'Amount',
+      }),
+    ]
   }),
-  new DynamicInputModel({
-      id: 'demoInput',
-      label: 'Amount',
-      maxLength: 51,
-      placeholder: 'Amount',
+  new DynamicFormGroupModel({
+    id: 'second-row',
+    group: [
+      new DynamicDatePickerModel({
+        id: 'demoDatePicker',
+        label: 'Date',
+        toggleLabel: 'Open',
+        placeholder: 'Date',
+        value: new Date(),
+        additional: {
+            containerClass: 'input-group-sm'
+        }
+      }),
+      new DynamicSelectModel<string>({
+          id: 'demoSelect2',
+          label: 'Status',
+          options: of([
+              {
+                  label: 'Bill',
+                  value: 'bill',
+              }
+          ])
+      }),
+      new DynamicInputModel({
+          id: 'demoInput2',
+          label: 'Paid',
+          maxLength: 51,
+          placeholder: 'Paid',
+      }),
+    ]
   }),
-  new DynamicDatePickerModel({
-
-      id: 'demoDatePicker',
-      label: 'Date',
-      toggleLabel: 'Open',
-      placeholder: 'Date',
-      value: new Date(),
-      additional: {
-          containerClass: 'theme-red'
-      }
+  new DynamicFormGroupModel({
+    id: 'third-row',
+    group: [
+      new DynamicInputModel({
+        id: 'demoInput3',
+        label: 'Pay',
+        maxLength: 51,
+        placeholder: 'Pay',
+      }),
+      new DynamicSelectModel<string>({
+          hint: 'Distribute the remaining amount to the loan lenders by % owned',
+          id: 'demoSelect3',
+          label: 'To',
+          options: of([
+              {
+                  label: 'Select lender',
+                  value: 'select-lender',
+              }
+          ])
+      }),
+      new DynamicInputModel({
+          id: 'demoInput4',
+          maxLength: 51,
+          label: 'Search',
+          placeholder: 'Search',
+      }),
+    ]
   }),
-  new DynamicSelectModel<string>({
-      id: 'demoSelect2',
-      label: 'Status',
-      options: of([
-          {
-              label: 'Bill',
-              value: 'bill',
-          }
-      ])
-  }),
-  new DynamicInputModel({
-      id: 'demoInput2',
-      label: 'Paid',
-      maxLength: 51,
-      placeholder: 'Paid',
-  }),
-  new DynamicInputModel({
-      id: 'demoInput3',
-      label: 'Pay',
-      maxLength: 51,
-      placeholder: 'Pay',
-  }),
-  new DynamicSelectModel<string>({
-      hint: 'Distribute the remaining amount to the loan lenders by % owned',
-      id: 'demoSelect3',
-      label: 'To',
-      options: of([
-          {
-              label: 'Select lender',
-              value: 'select-lender',
-          }
-      ])
-  }),
-  new DynamicInputModel({
-      id: 'demoInput4',
-      maxLength: 51,
-      label: 'Search',
-      placeholder: 'Search',
-  }),
-  new DynamicInputModel({
-      id: 'demoInput5',
-      label: 'Charge Interest',
-      maxLength: 51,
-      placeholder: 'Charge Interest',
-  }),
-  new DynamicDatePickerModel({
-      id: 'demoDatePicker2',
-      label: 'From Date',
-      toggleLabel: 'Open',
-      placeholder: 'From Date',
-      additional: {
-          containerClass: 'theme-red'
-      }
-  }),
-  new DynamicDatePickerModel({
-      id: 'demoDatePicker3',
-      label: 'to',
-      toggleLabel: 'Open',
-      placeholder: 'to',
-      additional: {
-          containerClass: 'theme-red'
-      }
+  new DynamicFormGroupModel({
+    id: 'fourth-row',
+    group: [
+      new DynamicInputModel({
+        id: 'demoInput5',
+        label: 'Charge Interest',
+        maxLength: 51,
+        placeholder: 'Charge Interest',
+      }),
+      new DynamicDatePickerModel({
+          id: 'demoDatePicker2',
+          label: 'From Date',
+          toggleLabel: 'Open',
+          placeholder: 'From Date',
+      }),
+      new DynamicDatePickerModel({
+          id: 'demoDatePicker3',
+          label: 'to',
+          toggleLabel: 'Open',
+          placeholder: 'to',
+      }),
+    ]
   }),
   new DynamicTextAreaModel({
       id: 'demoTextArea',
